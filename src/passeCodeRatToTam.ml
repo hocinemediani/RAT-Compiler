eb8fd2b572a2228811e1e22d42bb24c7d776f0f6
hocinemediani <dolcejeannarciss@gmail.com>
2026-01-09 00:27:01 +0100

    commentaires finis?

--- src/passeCodeRatToTam.ml
+++ src/passeCodeRatToTam.ml
@@ -1,4 +1,4 @@
-(* Module de la passe de gestion des identifiants *)
+(* Module de la passe de generation de code TAM *)
 (* doit être conforme à l'interface Passe *)
 open Tds
 open Ast
@@ -10,6 +10,14 @@ open Tam
 type t1 = AstPlacement.programme
 type t2 = string
 
+
+(**************************************************************************************)
+(* analyse_code_affectable_lecture : AstPlacement.affectable -> string                *)
+(* Parametre a : l'affectable à analyser en lecture.                                  *)
+(* Génère le code TAM pour charger la valeur d'un affectable (variable ou pointeur)   *)
+(* au sommet de la pile.                                                              *)
+(* Erreur si l'affectable n'est pas une variable accessible.                          *)
+(**************************************************************************************)
 let rec analyse_code_affectable_lecture a =
   match a with
   | AstType.Ident info -> 
@@ -19,6 +27,14 @@ let rec analyse_code_affectable_lecture a =
       | _ -> failwith "Erreur interne"
     end
 
+
+(**************************************************************************************)
+(* analyse_code_affectable_ecriture : AstPlacement.affectable -> string               *)
+(* Parametre a : l'affectable à analyser en écriture.                                 *)
+(* Génère le code TAM pour stocker la valeur au sommet de la pile vers l'adresse      *)
+(* de l'affectable.                                                                   *)
+(* Erreur si l'affectable n'est pas une variable accessible.                          *)
+(**************************************************************************************)
 let rec analyse_code_affectable_ecriture a =
   match a with
   | AstType.Ident info -> 
@@ -28,27 +44,25 @@ let rec analyse_code_affectable_ecriture a =
       | _ -> failwith "Erreur interne"
     end
 
-(* analyse_code_expression : tds -> AstSyntax.expression -> AstTds.expression *)
-(* Paramètre e : l'expression à analyser *)
-(* Vérifie la bonne utilisation des identifiants et tranforme l'expression
-en une expression de type AstTds.expression *)
-(* Erreur si mauvaise utilisation des identifiants *)
+
+(**************************************************************************************)
+(* analyse_code_expression : AstPlacement.expression -> string                        *)
+(* Parametre e : l'expression à analyser.                                             *)
+(* Génère le code TAM correspondant à l'évaluation de l'expression. Le résultat       *)
+(* sera placé au sommet de la pile.                                                   *)
+(* Erreur si mauvaise utilisation des identifiants.                                   *)
+(**************************************************************************************)
 let rec analyse_code_expression e =
   match e with
   | AstType.AppelFonction (info, le) ->
     begin
       match info_ast_to_info info with
       | InfoFun(s, _, _) ->
+        (* Evaluation des arguments de la gauche vers la droite puis appel *)
         (List.fold_right (fun e acc -> (analyse_code_expression e) ^ acc) le "") ^ (call "SB" (label s))
       | _ -> failwith "Erreur interne"
     end
   | AstType.Affectable a -> analyse_code_affectable_lecture a
-  (*| AstType.Ident info -> 
-    begin
-      match info_ast_to_info info with
-      | InfoVar(_, t, depl, reg) -> (load (getTaille t) depl reg)
-      | _ -> failwith "Erreur interne"
-    end *)
   | AstType.Booleen b -> 
     if b then (loadl_int 1)
     else (loadl_int 0)
@@ -69,36 +83,32 @@ let rec analyse_code_expression e =
       )
     end
 
-(* analyse_type_instruction : type -> info_ast option -> AstType.instruction -> Astplacement.instruction *)
-(* Paramètre oia : None si l'instruction i est dans le bloc principal,
-                   Some ia où ia est l'information associée à la fonction dans laquelle est l'instruction i sinon *)
-(* Paramètre i : l'instruction à analyser *)
-(* Vérifie la bonne utilisation des identifiants et tranforme l'instruction
-en une instruction de type Asttype.instruction *)
-(* Erreur si mauvaise utilisation des identifiants *)
+
+(**************************************************************************************)
+(* analyse_code_instruction : AstPlacement.instruction -> string                      *)
+(* Parametre i : l'instruction à analyser.                                            *)
+(* Génère le code TAM correspondant à l'exécution de l'instruction (gestion de pile,  *)
+(* sauts, étiquettes, appels systèmes).                                               *)
+(* Erreur si mauvaise utilisation des identifiants.                                   *)
+(**************************************************************************************)
 let rec analyse_code_instruction i =
   match i with
   | AstPlacement.Declaration (info, e) -> 
     begin
       match info_ast_to_info info with
       | InfoVar(_, t, depl, reg) -> 
+        (* Allocation sur la pile, evaluation de l'expression, stockage *)
         (push (getTaille t)) ^ (analyse_code_expression e) ^ (store (getTaille t) depl reg)
       | _ ->  failwith "Erreur interne"
     end
   | AstPlacement.Affectation (a, e) -> (analyse_code_expression e) ^ (analyse_code_affectable_ecriture a)
-  (*| AstPlacement.Affectation (info, e) -> 
-    begin
-      match info_ast_to_info info with
-      | InfoVar(_, t, depl, reg) -> 
-        (analyse_code_expression e) ^ (store (getTaille t) depl reg)
-      | _ ->  failwith "Erreur interne"
-    end*)
   | AstPlacement.AffichageInt e -> (analyse_code_expression e) ^ (subr (label "IOut"))
   | AstPlacement.AffichageRat e -> (analyse_code_expression e) ^ (call "SB" (label "rout"))
   | AstPlacement.AffichageBool e -> (analyse_code_expression e) ^ (subr (label "BOut"))
   | AstPlacement.Conditionnelle (c, t, e) ->
     let sinon = getEtiquette () in 
     let finsi = getEtiquette () in
+    (* Code : expression condition, saut si faux, bloc Then, saut fin, etiquette Sinon, bloc Else, etiquette Fin *)
     (analyse_code_expression c) ^ (jumpif 0 (label sinon)) ^ (analyse_code_bloc t) ^ (jump (label finsi)) ^ (label sinon) ^ (analyse_code_bloc e) ^ (label finsi)
   | AstPlacement.TantQue (c, b) ->
     let tantque = getEtiquette () in
@@ -108,35 +118,34 @@ let rec analyse_code_instruction i =
   | AstPlacement.Empty -> ""
 
 
-(* analyse_type_bloc : type -> info_ast option -> AstTds.bloc -> Asttype.bloc *)
-(* Paramètre oia : None si le bloc li est dans le programme principal,
-                   Some ia où ia est l'information associée à la fonction dans laquelle est le bloc li sinon *)
-(* Paramètre li : liste d'instructions à analyser *)
-(* Vérifie la bonne utilisation des identifiants et tranforme le bloc en un bloc de type Asttype.bloc *)
-(* Erreur si mauvaise utilisation des identifiants *)
+(**************************************************************************************)
+(* analyse_code_bloc : AstPlacement.bloc -> string                                    *)
+(* Parametre li : liste d'instructions à analyser avec la taille totale des locales.  *)
+(* Génère le code pour toutes les instructions du bloc et nettoie la pile (pop)       *)
+(* des variables locales à la fin.                                                    *)
+(* Erreur si mauvaise utilisation des identifiants.                                   *)
+(**************************************************************************************)
 and analyse_code_bloc (li, taille) =
   (List.fold_right(fun i acc -> (analyse_code_instruction i) ^ acc) li "") ^ (pop 0 taille)
 
 
-(* analyse_type_fonction : type -> AstTds.fonction -> Asttype.fonction *)
-(* Paramètre : la fonction à analyser *)
-(* Vérifie la bonne utilisation des identifiants et tranforme la fonction
-en une fonction de type Asttype.fonction *)
-(* Erreur si mauvaise utilisation des identifiants *)
+(**************************************************************************************)
+(* analyse_code_fonction : AstPlacement.fonction -> string                            *)
+(* Parametre : la fonction à analyser.                                                *)
+(* Génère l'étiquette de la fonction, le code de son corps, et l'instruction de fin.  *)
+(* Erreur si mauvaise utilisation des identifiants.                                   *)
+(**************************************************************************************)
 let analyse_code_fonction (AstPlacement.Fonction(info, _, bloc)) =
   match info_ast_to_info info with
   | InfoFun(n, _, _) -> (label n) ^ (analyse_code_bloc bloc) ^ halt
   | _ -> failwith "Erreur interne"
-  (* creer le label avec le nom de la fonction *)
-  (* analyse_code_bloc (li, taille) *)
-  (* ajouter halt *)
 
-  
 
-(* analyser : AstTds.programme -> AstType.programme *)
-(* Paramètre : le programme à analyser *)
-(* Vérifie la bonne utilisation des identifiants et tranforme le programme
-en un programme de type Asttype.programme *)
-(* Erreur si mauvaise utilisation des identifiants *)
+(**************************************************************************************)
+(* analyser : AstPlacement.programme -> string                                        *)
+(* Parametre : le programme à analyser.                                               *)
+(* Génère l'entête TAM, le code des fonctions, le code du main, et termine par halt.  *)
+(* Erreur si mauvaise utilisation des identifiants.                                   *)
+(**************************************************************************************)
 let analyser (AstPlacement.Programme (fonctions, prog)) =
   getEntete() ^ (List.fold_right(fun i acc -> (analyse_code_fonction i) ^ acc) fonctions "") ^ (label "main") ^ (analyse_code_bloc prog) ^ halt
--- src/passePlacementRat.ml
+++ src/passePlacementRat.ml
@@ -1,4 +1,4 @@
-(* Module de la passe de gestion des identifiants *)
+(* Module de la passe de placement memoire *)
 (* doit être conforme à l'interface Passe *)
 open Tds
 open Exceptions
@@ -9,28 +9,34 @@ open AstPlacement
 type t1 = Ast.AstType.programme
 type t2 = Ast.AstPlacement.programme
 
-(* analyse_type_instruction : type -> info_ast option -> AstType.instruction -> Astplacement.instruction *)
-(* Paramètre oia : None si l'instruction i est dans le bloc principal,
-                   Some ia où ia est l'information associée à la fonction dans laquelle est l'instruction i sinon *)
-(* Paramètre i : l'instruction à analyser *)
-(* Vérifie la bonne utilisation des identifiants et tranforme l'instruction
-en une instruction de type Asttype.instruction *)
-(* Erreur si mauvaise utilisation des identifiants *)
+
+(**************************************************************************************)
+(* analyse_placement_instruction : AstType.instruction -> int -> string ->            *)
+(* AstPlacement.instruction * int                     *)
+(* Parametre i : l'instruction à analyser.                                            *)
+(* Parametre depl : le déplacement courant par rapport au registre.                   *)
+(* Parametre reg : le registre courant (SB ou LB).                                    *)
+(* Calcule l'adresse relative des variables déclarées et transforme l'instruction.    *)
+(* Renvoie l'instruction placée et la taille occupée par les variables déclarées.     *)
+(* Erreur si mauvaise utilisation interne des identifiants.                           *)
+(**************************************************************************************)
 let rec analyse_placement_instruction i depl reg =
   match i with
   | AstType.Declaration (info, e) ->
     begin
       match info_ast_to_info info with
-      | InfoVar (_, t, _, _) -> modifier_adresse_variable depl reg info;
-                                (AstPlacement.Declaration(info, e), getTaille t)
+      | InfoVar (_, t, _, _) -> 
+        (* Mise à jour de l'adresse de la variable dans l'info_ast *)
+        modifier_adresse_variable depl reg info;
+        (AstPlacement.Declaration(info, e), getTaille t)
       | _ -> failwith "Erreur interne"
     end
   | AstType.Affectation (a, e) -> (AstPlacement.Affectation (a, e), 0)
-  (*| AstType.Affectation (info, e) -> (AstPlacement.Affectation (info, e), 0)*)
   | AstType.AffichageInt e -> (AstPlacement.AffichageInt (e), 0)
   | AstType.AffichageRat e -> (AstPlacement.AffichageRat (e), 0)
   | AstType.AffichageBool e -> (AstPlacement.AffichageBool (e), 0)
   | AstType.Conditionnelle (c, t, e) ->
+    (* Analyse des blocs sans modifier le déplacement courant pour la suite du programme *)
     let nt = analyse_placement_bloc t depl reg in
     let ne = analyse_placement_bloc e depl reg in
     (AstPlacement.Conditionnelle (c, nt, ne), 0)
@@ -40,32 +46,43 @@ let rec analyse_placement_instruction i depl reg =
   | AstType.Retour (e, ia) ->
     begin
       match info_ast_to_info ia with
-      | InfoFun(_, tr, tp) -> (AstPlacement.Retour (e, getTaille tr, (List.fold_right (fun t tq -> tq + (getTaille t)) tp 0)), 0)
+      | InfoFun(_, tr, tp) -> 
+        (* Calcul de la taille des paramètres pour le nettoyage de la pile *)
+        (AstPlacement.Retour (e, getTaille tr, (List.fold_right (fun t tq -> tq + (getTaille t)) tp 0)), 0)
       | _ -> failwith "Erreur interne"
     end
   | AstType.Empty -> (AstPlacement.Empty, 0)
 
 
-(* analyse_type_bloc : type -> info_ast option -> AstTds.bloc -> Asttype.bloc *)
-(* Paramètre oia : None si le bloc li est dans le programme principal,
-                   Some ia où ia est l'information associée à la fonction dans laquelle est le bloc li sinon *)
-(* Paramètre li : liste d'instructions à analyser *)
-(* Vérifie la bonne utilisation des identifiants et tranforme le bloc en un bloc de type Asttype.bloc *)
-(* Erreur si mauvaise utilisation des identifiants *)
+(**************************************************************************************)
+(* analyse_placement_bloc : AstType.bloc -> int -> string -> AstPlacement.bloc * int  *)
+(* Parametre li : liste d'instructions à analyser.                                    *)
+(* Parametre depl : le déplacement initial dans le bloc.                              *)
+(* Parametre reg : le registre courant.                                               *)
+(* Parcourt les instructions en mettant à jour le déplacement au fur et à mesure      *)
+(* des déclarations. Renvoie le bloc placé et la taille totale allouée par le bloc.   *)
+(* Erreur si mauvaise utilisation des identifiants.                                   *)
+(**************************************************************************************)
 and analyse_placement_bloc li depl reg =
   match li with
   | [] -> ([], 0)
-  | t::q -> let (nt, tt) = analyse_placement_instruction t depl reg in
-            let (nq, tq) = analyse_placement_bloc q (depl + tt) reg in
-            (nt::nq, tt + tq)
+  | t::q -> 
+      (* Analyse de l'instruction courante *)
+      let (nt, tt) = analyse_placement_instruction t depl reg in
+      (* Analyse de la suite avec le déplacement mis à jour *)
+      let (nq, tq) = analyse_placement_bloc q (depl + tt) reg in
+      (nt::nq, tt + tq)
 
 
-(* analyse_type_fonction : type -> AstTds.fonction -> Asttype.fonction *)
-(* Paramètre : la fonction à analyser *)
-(* Vérifie la bonne utilisation des identifiants et tranforme la fonction
-en une fonction de type Asttype.fonction *)
-(* Erreur si mauvaise utilisation des identifiants *)
+(**************************************************************************************)
+(* analyse_placement_fonction : AstType.fonction -> AstPlacement.fonction             *)
+(* Parametre : la fonction à analyser.                                                *)
+(* Place les paramètres (déplacement négatif par rapport à LB) et les variables       *)
+(* locales (déplacement positif par rapport à LB).                                    *)
+(* Erreur si mauvaise utilisation des identifiants.                                   *)
+(**************************************************************************************)
 let analyse_placement_fonction (AstType.Fonction(info, lp, li)) =
+  (* Fonction auxiliaire pour placer les paramètres en remontant depuis LB *)
   let rec placement_variables lvar depl =
     match lvar with
     | [] -> ()
@@ -73,22 +90,27 @@ let analyse_placement_fonction (AstType.Fonction(info, lp, li)) =
       begin
         match info_ast_to_info ia with
         | InfoVar (_, t, _, _) ->
+          (* Calcul du déplacement : LB - taille totale paramètres + offset *)
           modifier_adresse_variable (depl - getTaille t) "LB" ia;
           placement_variables q (depl - getTaille t)
         | _ -> failwith "Erreur interne"
       end
-    in let _ = placement_variables (List.rev lp) 0 in
+    in 
+  (* Placement des paramètres *)
+  let _ = placement_variables (List.rev lp) 0 in
+  (* Placement du corps de la fonction : les locales commencent à LB + 3 (après le chainage statique, etc) *)
   let nli = analyse_placement_bloc li 3 "LB" in
   AstPlacement.Fonction(info, lp, nli)
   
-  
 
-(* analyser : AstTds.programme -> AstType.programme *)
-(* Paramètre : le programme à analyser *)
-(* Vérifie la bonne utilisation des identifiants et tranforme le programme
-en un programme de type Asttype.programme *)
-(* Erreur si mauvaise utilisation des identifiants *)
+(**************************************************************************************)
+(* analyser : AstType.programme -> AstPlacement.programme                             *)
+(* Parametre : le programme à analyser.                                               *)
+(* Lance le placement des fonctions et du bloc principal (sur SB).                    *)
+(* Erreur si mauvaise utilisation des identifiants.                                   *)
+(**************************************************************************************)
 let analyser (AstType.Programme (fonctions, prog)) =
   let nf = List.map (analyse_placement_fonction) fonctions in
+  (* Le bloc principal commence à l'adresse 0 du registre SB *)
   let nb = analyse_placement_bloc prog 0 "SB" in
   AstPlacement.Programme (nf, nb)
